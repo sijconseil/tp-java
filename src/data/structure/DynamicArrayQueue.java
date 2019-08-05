fd4f23e8ff794e8fac68add0b91aefeaaeec1ef4
Fabien JALABERT <fabien.jalabert@gmail.com>
2019-08-05 15:11:23 +0200

    commit

--- src/data/structure/DynamicArrayQueue.java
+++ src/data/structure/DynamicArrayQueue.java
@@ -26,30 +26,61 @@ public class DynamicArrayQueue<T> implements Queue<T> {
 	
 	private void checkSize() {
 		// test si place disponible
-		
+		int tmpSize = size();
+		if(tmpSize<array.length-2) return;
+
 		// si pas de place, agrandit le tableau avec une copie "spéciale"
+		DynamicArrayQueue<T> newQueue = new DynamicArrayQueue<>(array.length*2);
+		for(int i=0;i<tmpSize;i++) newQueue.enqueue(this.dequeue());
+		
+		this.array = newQueue.array;
+		this.startIndex = newQueue.startIndex;
+		this.endIndex = newQueue.endIndex;
+	
+	}
+	
+	private void checkSizeV2() {
+		// test si place disponible
+		int tmpSize = size();
+		if(tmpSize<array.length-2) return;
+
+		// si pas de place, agrandit le tableau avec une copie "spéciale"
+		T[] newArray= (T[])new Object[array.length*2];
+		int currentIndex = startIndex-1;
+		for(int i = 0;i<tmpSize;i++) {
+			newArray[i]=array[currentIndex];
+			currentIndex = getNextIndex(currentIndex);
+		}
+		this.startIndex = 0;
+		this.endIndex = tmpSize;
+		this.array = newArray;
 	}
 	
 	private int getNextIndex(int index) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(index==array.length) {
+			return 0;
+		}
+		return index+1;
 	}
 	
 	@Override
 	public void enqueue(T value) {
-		// TODO Auto-generated method stub
+		array[endIndex] = value;
+		endIndex = getNextIndex(endIndex);
 	}
 
 	@Override
 	public T dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		T result = array[startIndex];
+		array[startIndex]=null; // dereferencer l'objet pour qu'il soit traité par le garbage collector (gc)
+		startIndex = getNextIndex(startIndex);
+		return result;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(endIndex >startIndex) return endIndex - startIndex;
+		return array.length - (startIndex - endIndex);
 	}
 
 }
