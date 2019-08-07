c630ae86b0ff31e5e6c81264aa17c3ecf922abd0
Fabien JALABERT <fabien.jalabert@gmail.com>
2019-08-07 22:32:38 +0200

    commit

--- src/lettertree/LetterTree.java
+++ src/lettertree/LetterTree.java
@@ -48,23 +48,73 @@ public class LetterTree {
 	
 	
 	public boolean contains(String word) {
-		// TODO
-		return false;
+		// solution 1 itérative: similaire au add...
+		
+		Node current = root;
+		char[] chars = word.toCharArray();
+		for(int i=0;i<chars.length;i++) {
+			if(!current.children.containsKey(chars[i])) {
+				return false;
+			}
+			current = current.children.get(chars[i]);
+		}
+		return current.terminal;
+	}
+	
+	public boolean containsRecursive(String word) {
+		return containsRecursive(word, 0, root);
+	}
+	
+	private boolean containsRecursive(String word, int currentIndex, Node currentNode) {
+		if(currentIndex==word.length()) return currentNode.terminal;
+		char currentChar = word.charAt(currentIndex);
+		Node child = currentNode.children.get(currentChar);
+		if(child==null) return false;
+		return containsRecursive(word, currentIndex+1, child);
 	}
 	
 	public int countNodes() { // retourne le nombre de noeud de l'arbre
-		// TODO
-		return 0;
+		return countNodes(root);
+	}
+	private int countNodes(Node currentNode) {
+		int total= 1;
+		for(Node node: currentNode.children.values()) {
+			total+=countNodes(node);
+		}
+		return total;
 	}
 	
 	public int maxDepth() { // retourne la profondeur maximum de l'arbre
-		// TODO
-		return 0;
+		return maxDepth(root);
+	}
+	private int maxDepth(Node currentNode) { // retourne la profondeur maximum de l'arbre
+		// on suite le meme principe de parcours que countNodes et countWords, 
+		// la seule différence est que ON RECHERCHE LA MAX DES FILS DANS LA RECURSION
+		
+		// petit rappel :il est parfois plus facile de commencer par la condition d'arret:
+		// c'est à dire penser en premier à ce que fait le noeud feuille,
+		// ensuite réfléchir à ce que doit faire sont parent, puis le parent du parent
+		// et vérifier que ca fonctionne dans le cas général
+		if(currentNode.children.isEmpty())return 1;
+		int max = -1;
+		for(Node node : currentNode.children.values()) {
+			int tmpDepth = maxDepth(node);
+			if(tmpDepth>max) max = tmpDepth;
+		}
+		return max+1;
 	}
 	
 	public int countWords() {// retourne le nombre de mots stockés dans l'arbre
-		// TODO
-		return 0;
+		return countWords(root);
+	}
+	private int countWords(Node currentNode) {
+		// la seule chose qui change est la condition d'etre terminal pour ajouter 1 au total
+		int total= currentNode.terminal?1:0;
+		// le reste est indentique à countNodes
+		for(Node node: currentNode.children.values()) {
+			total+=countNodes(node);
+		}
+		return total;
 	}
 	
 	public void load(String filePath) { // charge le fichier dont le chemin est donné en paramètre
--- src/lettertree/LetterTreeMain.java
+++ src/lettertree/LetterTreeMain.java
@@ -4,16 +4,16 @@ public class LetterTreeMain {
 	public static void main(String[] args) {
 		LetterTree dico = new LetterTree();
 		dico.add("mai");
-		dico.display();
 		dico.add("maison");
-		dico.display();
 		dico.add("mais");
-		dico.display();
 		dico.add("mois");
-		dico.display();
 		dico.remove("mai");
-		dico.display();
 		dico.remove("maison");
 		dico.display();
+		System.out.println(dico.containsRecursive("mois"));
+		System.out.println(dico.containsRecursive("moi"));
+		System.out.println(dico.containsRecursive("maison"));
+		System.out.println(dico.containsRecursive("maisonnette"));
+
 	}
 }
